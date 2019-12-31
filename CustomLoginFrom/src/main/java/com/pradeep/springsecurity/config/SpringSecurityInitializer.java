1701f560b4fbb0a813b55a5322261059b3043599
PradeepKumarHE <pradeepkumarhe1989@gmail.com>
2019-12-31 09:10:05 +0530

    Add files via upload

---
+++ CustomLoginFrom/src/main/java/com/pradeep/springsecurity/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.pradeep.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer   extends AbstractSecurityWebApplicationInitializer {
+
+}
